d5d0852fc1cf12d9fcc4762e9372b6dfdf30fdd0
ianygg <yangjinjian811916@163.com>
2025-03-25 15:25:55 +0800

    1.0.0-rc.8.16

--- src/modules/checkout/pages/frontStore/all/[auth]addCustomerToCart.js
+++ src/modules/checkout/pages/frontStore/all/[auth]addCustomerToCart.js
@@ -1,10 +1,20 @@
-const { select, update } = require('@evershop/postgres-query-builder');
-const { pool } = require('@evershop/evershop/src/lib/postgres/connection');
-const { debug } = require('@evershop/evershop/src/lib/log/debuger');
+const {
+  select,
+  update,
+  del
+} = require('@evershop/postgres-query-builder');
+const {
+  pool
+} = require('@evershop/evershop/src/lib/postgres/connection');
+const {
+  debug
+} = require('@evershop/evershop/src/lib/log/debuger');
 
 module.exports = async (request, response, delegate, next) => {
   try {
-    const { sessionID } = request;
+    const {
+      sessionID
+    } = request;
     const customer = request.getCurrentCustomer();
     if (customer) {
       // Check if there is any cart with the same sid
@@ -14,6 +24,24 @@ module.exports = async (request, response, delegate, next) => {
         .and('status', '=', 1)
         .load(pool);
       if (cart) {
+        //查看当前账户下是否存在status = 1 的购物车
+        const customerCart = await select()
+          .from('cart')
+          .where('customer_id', '=', customer.customer_id)
+          .and('status', '=', 1)
+          .load(pool);
+        if (customerCart && customerCart.cart_id != cart.cart_id) {
+          //将当前cusstomerCart清除掉，并将cart_item更新到
+          await update('cart_item')
+            .given({
+              cart_id: cart.cart_id
+            })
+            .where('cart_id', '=', customerCart.cart_id)
+            .execute(pool);
+          await del('cart')
+            .where('cart_id', '=', customerCart.cart_id)
+            .execute(pool);
+        }
         await update('cart')
           .given({
             customer_group_id: customer.group_id,
