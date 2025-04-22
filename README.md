89b040d7a7c778dda111c7adcd92060e5dad5fc6
ianygg <yangjinjian811916@163.com>
2025-04-22 17:24:29 +0800

    update 8.16

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 order_placed
 
 ```
+
+```
+1.0.0-rc.8.16
+src\modules\checkout\pages\frontStore\all\[context]detectCurrentCart[auth].js
+
+```
--- src/modules/checkout/pages/frontStore/all/[context]detectCurrentCart[auth].js
+++ src/modules/checkout/pages/frontStore/all/[context]detectCurrentCart[auth].js
@@ -1,9 +1,16 @@
-const { select, update } = require('@evershop/postgres-query-builder');
-const { pool } = require('@evershop/evershop/src/lib/postgres/connection');
+const {
+  select,
+  update
+} = require('@evershop/postgres-query-builder');
+const {
+  pool
+} = require('@evershop/evershop/src/lib/postgres/connection');
 const {
   setContextValue
 } = require('../../../../graphql/services/contextHelper');
-
+const {
+  getCartByUUID
+} = require('@evershop/evershop/src/modules/checkout/services/getCartByUUID');
 module.exports = async (request, response, delegate, next) => {
   // Check if any cart is associated with the session id
   const cart = await select()
@@ -11,9 +18,45 @@ module.exports = async (request, response, delegate, next) => {
     .where('sid', '=', request.sessionID)
     .andWhere('status', '=', 1)
     .load(pool);
-
   if (cart) {
-    setContextValue(request, 'cartId', cart.uuid);
+    //将BuyNow 的cart进行处理
+    let uuid = cart.uuid;
+    let _uuid = cart.user_ip;
+
+    if (_uuid) {
+      if (_uuid == 'buynow') {
+        await update('cart')
+          .given({
+            status: 0,
+            user_ip: null
+          })
+          .where('uuid', '=', uuid)
+          .execute(pool);
+      } else {
+        let _cart = await select()
+          .from('cart')
+          .where('uuid', '=', _uuid)
+          .andWhere('status', '=', 0)
+          .load(pool);
+        if (_cart) {
+          await update('cart')
+            .given({
+              status: 1
+            })
+            .where('uuid', '=', _cart.uuid)
+            .execute(pool);
+          await update('cart')
+            .given({
+              status: 0,
+              user_ip: null
+            })
+            .where('uuid', '=', cart.uuid)
+            .execute(pool);
+          uuid = _cart.uuid;
+        }
+      }
+    }
+    setContextValue(request, 'cartId', uuid);
   } else {
     // Get the customer id from the session
     const customerID = request.session.customerID || null;
@@ -28,7 +71,9 @@ module.exports = async (request, response, delegate, next) => {
       if (customerCart) {
         // Update the cart with the session id
         await update('cart')
-          .given({ sid: request.sessionID })
+          .given({
+            sid: request.sessionID
+          })
           .where('uuid', '=', customerCart.uuid)
           .execute(pool);
         request.session.cartID = customerCart.uuid;
