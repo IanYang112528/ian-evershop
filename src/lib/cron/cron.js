0c62340b2e868ff19ba6cc66f907f0e18d003136
ianygg <yangjinjian811916@163.com>
2025-12-03 14:56:33 +0800

    update 1.0.0-rc.8.26

--- src/lib/cron/cron.js
+++ src/lib/cron/cron.js
@@ -4,14 +4,8 @@ const {
 } = require('@evershop/evershop/src/lib/event/emitter');
 async function start() {
     // 每2天执行一次 (0 0 */2 * *)
-    if (process.env.GAGAB2B_CRON) {
-        cron.schedule(process.env.GAGAB2B_CRON, async () => {
-            try {
-                await emit('upload_giga_product', +new Date())
-            } catch (error) {
-                console.error('定时任务执行失败:', error.message);
-            }
-        });
-    }
+    await emit('upload_giga_product', {
+        data: 'upload_giga_product'
+    })
 }
 start();
